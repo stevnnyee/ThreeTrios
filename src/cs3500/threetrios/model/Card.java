f3352015263c448289e059dd51578f079390ecf6
Steven <stevnn.yee@gmail.com>
2024-11-13 00:51:01 -0500

    added javadocs for grid and card interfaces and classes. added a few helper methods and interface methods new configuration files updates config files updated model updated files and readme new javadocs finalizing javadocs finalized tests

--- src/cs3500/threetrios/model/Card.java
+++ src/cs3500/threetrios/model/Card.java
@@ -4,7 +4,9 @@ package cs3500.threetrios.model;
  * Behaviors for a Card in the Game of ThreeTrios.
  * Any additional behaviors for cards must be made
  * creating a new interface that extends this one.
- * Interface for a Card in the ThreeTrios game. 
+ * Interface for a Card in the ThreeTrios game.
+ * A ThreeTrios card is different from a RedSeven card in which it not only has an attack power,
+ * but 4 directions which has its distinct directional power.
  */
 public interface Card {
 
